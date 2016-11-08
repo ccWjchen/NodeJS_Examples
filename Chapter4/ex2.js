db523791448544a4df8a6b5ee96d83566484a607
ccWjchen <cc.wjchen@gmail.com>
2016-11-08 15:10:06 +0800

    add ex
    chapter4\ex2 ex3

---
+++ Chapter4/ex2.js
@@ -0,0 +1,4 @@
+if (console==global.console) {
+  global.myDogCount=3;
+  console.log(myDogCount);
+}
---
+++ Chapter4/ex3.js
@@ -0,0 +1,7 @@
+﻿var fs=require("fs");
+const output = fs.createWriteStream('./stdout.log');
+const errorOutput = fs.createWriteStream('./stderr.log');
+
+const logger = new console.Console(output, errorOutput);
+logger.log("count=%d", 254);
+logger.error("錯誤");
